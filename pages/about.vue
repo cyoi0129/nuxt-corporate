875a647af3255cddd8997467426d9eed11eeb160
cyoi <cyoi0129@gmail.com>
2022-01-03 17:38:06 +0900

    Add contents to other pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>About</h1>
+    <Timeline />
   </div>
 </template>
 <script lang="ts">
@@ -9,3 +10,24 @@ export default Vue.extend({
   name: "About",
 });
 </script>
+<style lang="scss" scoped>
+section {
+  padding: 16px;
+  img {
+    width: 100%;
+  }
+  &.flex-content {
+    @media only screen and (min-width: 920px) {
+      display: flex;
+      article {
+        width: 50%;
+      }
+    }
+    @media only screen and (max-width: 919px) {
+      article {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
--- pages/recult.vue
+++ pages/recult.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
     <h1>Recult</h1>
+    <section class="flex-content">
+      <article><img src="../static/analysis.jpg" alt="" /></article>
+      <TextBlock />
+    </section>
   </div>
 </template>
 <script lang="ts">
@@ -9,3 +13,24 @@ export default Vue.extend({
   name: "Recult",
 });
 </script>
+<style lang="scss" scoped>
+section {
+  padding: 16px;
+  img {
+    width: 100%;
+  }
+  &.flex-content {
+    @media only screen and (min-width: 920px) {
+      display: flex;
+      article {
+        width: 50%;
+      }
+    }
+    @media only screen and (max-width: 919px) {
+      article {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
